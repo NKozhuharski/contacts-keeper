b6cb985c38c5a08fe919f3f28678044d295200e7
Nikolay Kozhuharski <nikolay.kozhuharski@softwaregroup.com>
2022-01-08 09:21:12 +0200

    Prepare for deploy.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
-var cors = require('cors')
+var cors = require('cors');
+const { path } = require('express/lib/application');
 
 const app = express();
 
@@ -16,6 +17,12 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT =process.env.PORT || 5000;
 
